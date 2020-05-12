ac3ad1041dd63e45a6ebeddbb3b7ee416c2a1ffd
admin142 <admin@142.com>
2020-05-12 15:23:35 +0800

    vponUtilDone

---
+++ gRTB/protoHelper.cs
@@ -0,0 +1,61 @@
+﻿using Google.Protobuf;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gRTB
+{
+    public class ProtoMessageConverter : JsonConverter
+    {
+        /// <summary>
+        /// Called by NewtonSoft.Json's method to ask if this object can serialize
+        /// an object of a given type.
+        /// </summary>
+        /// <returns>True if the objectType is a Protocol Message.</returns>
+        public override bool CanConvert(System.Type objectType)
+        {
+            return typeof(Google.Protobuf.IMessage)
+                .IsAssignableFrom(objectType);
+        }
+
+        /// <summary>
+        /// Reads the json representation of a Protocol Message and reconstructs
+        /// the Protocol Message.
+        /// </summary>
+        /// <param name="objectType">The Protocol Message type.</param>
+        /// <returns>An instance of objectType.</returns>
+        public override object ReadJson(JsonReader reader,
+            System.Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            // The only way to find where this json object begins and ends is by
+            // reading it in as a generic ExpandoObject.
+            // Read an entire object from the reader.
+            var converter = new ExpandoObjectConverter();
+            object o = converter.ReadJson(reader, objectType, existingValue,
+                serializer);
+            // Convert it back to json text.
+            string text = JsonConvert.SerializeObject(o);
+            // And let protobuf's parser parse the text.
+            IMessage message = (IMessage)Activator
+                .CreateInstance(objectType);
+            return Google.Protobuf.JsonParser.Default.Parse(text,
+                message.Descriptor);
+        }
+
+        /// <summary>
+        /// Writes the json representation of a Protocol Message.
+        /// </summary>
+        public override void WriteJson(JsonWriter writer, object value,
+            JsonSerializer serializer)
+        {
+            // Let Protobuf's JsonFormatter do all the work.
+            writer.WriteRawValue(Google.Protobuf.JsonFormatter.Default
+                .Format((IMessage)value));
+        }
+    }
+}
--- proto/Program.fs
+++ proto/Program.fs
@@ -1,20 +1,109 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module vpon.protobuf
 
 open Google.Protobuf
 open System.IO
+open System
+open Newtonsoft.Json
+open gRTB
+open Protobuf.Comm.Doubleclick
+open Newtonsoft.Json.Linq
+
+
+let bArr2Json (bArr:byte[]) =
+    try
+        let br = new BidRequest()
+        br.MergeFrom(bArr) //read message from protodat file
+        let formatter = 
+            new JsonFormatter(
+                new JsonFormatter.Settings(false)
+                )
+        let jsonString = formatter.Format(br)
+        let parsedJson = JToken.Parse(jsonString)
+        let beautified = parsedJson.ToString(Formatting.Indented)
+        beautified
+    with
+    | exn ->
+        exn.Message
+
+let bin2json p =
+    try
+        let input = File.OpenRead(p) 
+        let bArr = Array.zeroCreate (int input.Length)
+        ignore <| input.Read(bArr, 0, int input.Length)
+        bArr2Json bArr
+    with
+    | exn ->
+        exn.Message
+
+
+let brArr2Json (bArr:byte[]) =
+    try
+        let br = new BidResponse()
+        br.MergeFrom(bArr) //read message from protodat file
+        let formatter = 
+            new JsonFormatter(
+                new JsonFormatter.Settings(false)
+                )
+        let jsonString = formatter.Format(br)
+        let parsedJson = JToken.Parse(jsonString)
+        let beautified = parsedJson.ToString(Formatting.Indented)
+        beautified
+    with
+    | exn ->
+        exn.Message
+
+
+
+let json2bin jsonString p =
+    try 
+        let brObj = JsonConvert.DeserializeObject<BidRequest>(jsonString, new ProtoMessageConverter())
+        let fi = new FileInfo(p)
+        if fi.Exists then fi.Delete()
+        let fs = new FileStream(p, FileMode.CreateNew)
+        brObj.WriteTo fs
+        fs.Flush()
+        fs.Close()
+        fs.Dispose()
+    with
+    | exn ->
+        printfn "%s" exn.Message
+
+
+
+
 
 [<EntryPoint>]
 let main argv =
-    let input = File.OpenRead("") 
-    
-    let br = new BidRequest()
-    br.MergeFrom(input) //read message from protodat file
-    let formatter = 
-        new JsonFormatter(
-            new JsonFormatter.Settings(false)
-            )
-    let jsonString = formatter.Format(br)
-    System.IO.File.WriteAllText("", jsonString)
-    
+    //let input = File.OpenRead(@"Z:\froto\proto\noPmpEncode") 
+    //let bArr = Array.zeroCreate (int input.Length)
+    //ignore <| input.Read(bArr, 0, int input.Length)
+    //let br = new BidRequest()
+    //br.MergeFrom(bArr) //read message from protodat file
+    //let formatter = 
+    //    new JsonFormatter(
+    //        new JsonFormatter.Settings(false)
+    //        )
+    //let jsonString = formatter.Format(br)
+    let jsonString = bin2json @"Z:\froto\proto\noPmpEncode"
+    System.IO.File.WriteAllText(@"./br.json", jsonString)
+    printfn "%s" jsonString
+
+    let brObj = JsonConvert.DeserializeObject<BidRequest>(jsonString, new ProtoMessageConverter())
+    //let js = JsonSerializer.Create()
+    //let jr = JsonReader.
+    //let brObj = js.Deserialize<BidRequest>(jsonString)
+    printfn "%A" brObj
+
+
+    json2bin jsonString @".\protoBin"
+
+    let jsonString2 = bin2json @".\protoBin"
+    System.IO.File.WriteAllText(@"./br2.json", jsonString2)
+
+
+    let jStr = System.IO.File.ReadAllText @"./brPMP.json"
+    json2bin jStr @".\protoBinPmp"
+
+    System.Console.ReadLine() |> ignore
     0 // return an integer exit code
+    
